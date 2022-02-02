4db03df72da41e2342b556c48e33506e15ae0d7e
Emre Coşkunçay <emrecoskuncay@gmail.com>
2022-02-02 11:09:40 +0300

    getWordOfTheDay changed

--- src/words.ts
+++ src/words.ts
@@ -18,9 +18,7 @@ export function getWordOfTheDay() {
   const start = new Date(2022, 0, 0)
   const diff = Number(now) - Number(start)
   let day = Math.floor(diff / (1000 * 60 * 60 * 24))
-  while (day > answers.length) {
-    day -= answers.length
-  }
+  day = day % answers.length
   return answers[day]
 }
 
